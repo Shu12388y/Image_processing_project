7a46282d8ad353e25beae1237051fb1a2ec0a44b
Shu12388y <spl.sp999@gmail.com>
2023-08-25 17:05:16 +0530

    histrogram plot

---
+++ project_folder/example1.m
@@ -0,0 +1,33 @@
+clc
+close all
+a=imread('C:\Users\shubham\Downloads\photo-1542840843-3349799cded6.jpg');
+figure 
+imshow(a)
+b=rgb2gray(a);
+figure
+imshow(b)
+k=imhist(b);
+
+% for i=1:length(k)
+  %  p=k(i)/256;
+   % disp(p)
+   %end%
+
+min=5;
+max=192;
+k2=double(k);
+
+
+% Apply the formula.
+k3=(k2-min)./(max-min);
+ 
+% Multiply with maximum possible value.
+k4=k3.*256;
+ 
+% Convert the final result into uint8.
+ k5=uint8(k4);
+ 
+% Display the enhanced image and histogram.
+imhist(k5);
+
+
